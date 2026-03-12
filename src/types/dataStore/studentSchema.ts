51a744e12085534f672da04de09ae585947bc4a5
jahnical <jahnics1@gmail.com>
2026-03-12 19:28:47 +0200

    feat(types): add studentIdentifier to admission schema

--- src/types/dataStore/studentSchema.ts
+++ src/types/dataStore/studentSchema.ts
@@ -70,7 +70,8 @@ const transferSchema = z.object({
 
 const admissionSchema = z.object({
     enabled: z.boolean().optional(),
-    admissionDate: z.string().optional()
+    admissionDate: z.string().optional(),
+    studentIdentifier: z.string().optional()
 });
 
 export const studentDataStore = z.object({
